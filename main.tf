9da4a3e78eaf956121fc06d8d50fc62fcacb9e98
maugust17 <maugust07@gmail.com>
2022-08-05 15:26:11 -0300

    Cluster Kubernetes

--- main.tf
+++ main.tf
@@ -10,3 +10,16 @@ terraform {
 provider "digitalocean" {
   token = "dop_v1_3aad1c0c14c81a0ae2d92b1629b92e67a443a9e42579a4675ba75f4ee90755cc"
 }
+
+resource "digitalocean_kubernetes_cluster" "k8s_iniciativa" {
+  name   = "k8s-iniciativa"
+  region = "nyc1"
+  # Grab the latest version slug from `doctl kubernetes options versions`
+  version = "1.23.9-do.0"
+
+  node_pool {
+    name       = "default"
+    size       = "s-2vcpu-2gb"
+    node_count = 3
+  }
+}

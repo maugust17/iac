57c6ac8be46675fc47f9aac6bc9f34c22be8fe85
maugust17 <maugust07@gmail.com>
2022-08-05 16:06:12 -0300

    Var

--- main.tf
+++ main.tf
@@ -8,12 +8,15 @@ terraform {
 }
 # Configure the DigitalOcean Provider
 provider "digitalocean" {
-  token = "dop_v1_06c98b15ff9a764bf43fbe2a320cd7b74290303d36e10ad965bfaa545326a4e7"
+  token = var.do_token
+  #"dop_v1_06c98b15ff9a764bf43fbe2a320cd7b74290303d36e10ad965bfaa545326a4e7"
 }
 
 resource "digitalocean_kubernetes_cluster" "k8s_iniciativa" {
-  name   = "k8s-iniciativa"
-  region = "nyc1"
+  name   = var.k8s_name
+  #"k8s-iniciativa"
+  region = var.region
+  #"nyc1"
   # Grab the latest version slug from `doctl kubernetes options versions`
   version = "1.23.9-do.0"
 
@@ -30,5 +33,15 @@ resource "digitalocean_kubernetes_node_pool" "node_adicional" {
   name       = "backend-pool"
   size       = "s-2vcpu-2gb"
   node_count = 1
+  
+}
+
+variable "do_token" {
+  
+}
+variable "k8s_name" {
+  
+}
+variable "region" {
   
 }
